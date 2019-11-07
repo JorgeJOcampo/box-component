8e5fb52b1d4e63997eadec85b2a0fd5068b8b065
Shorshi <jorgejocampo@gmail.com>
2019-11-07 00:39:30 -0300

    First attemp with dark mode

---
+++ src/components/Box.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Box.css';
+
+export default () => <div className="container">
+  <div className="box">
+    Jorge
+  </div>
+  <div className="box">
+    Maru
+  </div>
+  <div className="box">
+    S
+  </div>
+</div>
